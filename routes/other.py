f7cc0eca054f232628d1dd4c3bc397afba900f44
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-02-20 02:45:17 +0000

    still trying

--- routes/other.py
+++ routes/other.py
@@ -778,16 +778,12 @@ def team():
 import os, sys, subprocess, platform
 
 def _get_pdfkit_config():
-    """wkhtmltopdf lives and functions differently depending on Windows or Linux. We
-     need to support both since we develop on windows but deploy on Heroku.
-
-    Returns:
-        A pdfkit configuration
-    """
-    if platform.system() == 'Windows':
-        return pdfkit.configuration(wkhtmltopdf=os.environ.get('WKHTMLTOPDF_BINARY', r'.bin\wkhtmltopdf.exe'))
+    if platform.system() == "Windows":
+        return pdfkit.configuration(wkhtmltopdf=os.environ.get('WKHTMLTOPDF_BINARY', r'.\bin\wkhtmltopdf.exe'))
     else:
-        WKHTMLTOPDF_CMD = subprocess.Popen(['which', os.environ.get('WKHTMLTOPDF_BINARY', 'wkhtmltopdf')], stdout=subprocess.PIPE).communicate()[0].strip()
+        os.environ['PATH'] += os.pathsep + os.path.dirname(sys.executable)
+        WKHTMLTOPDF_CMD = subprocess.Popen(['which', os.environ.get('WKHTMLTOPDF_BINARY', 'wkhtmltopdf')],
+            stdout=subprocess.PIPE).communicate()[0].strip()
         return pdfkit.configuration(wkhtmltopdf=WKHTMLTOPDF_CMD)
 
 
