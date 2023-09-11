481db6a80b6d401ee2c8c6d56d774fa45b69784b
unique-red <noah13victor@gmail.com>
2023-09-11 21:28:41 +0100

    app context for new db

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from app_config import create_app
+from app_config import create_app, db
 from extensions import sess
+from models import User, Invitees, Beneficiary, Transaction, Card
 
 app = create_app()
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Invitees': Invitees, 'Beneficiary': Beneficiary, 'Transaction': Transaction, 'Card': Card}
 
 if __name__ == "__main__":
     app.secret_key = 'super secret key'
