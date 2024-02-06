3ac1beb3ef90ec25650327ea2e27aa9f5ec132bc
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-02-06 21:07:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from app_config import create_app, db
 from extensions import sess
 from models import User, Invitees, Beneficiary, Transaction, Card
+from dotenv import load_dotenv
+
 
 app = create_app()
 
+load_dotenv()
+
+
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Invitees': Invitees, 'Beneficiary': Beneficiary, 'Transaction': Transaction, 'Card': Card}
+    return {'db': db, 'User': User, 'Invitees': Invitees, 'Beneficiary': Beneficiary, 'Transaction': Transaction,
+            'Card': Card}
+
 
 if __name__ == "__main__":
     app.secret_key = 'super secret key'
