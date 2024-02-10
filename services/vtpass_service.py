d33961da099913aa88f39a336adfac23af342c87
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-02-10 22:40:34 +0100

    Update vtpass_service.py

---
+++ services/vtpass_service.py
@@ -0,0 +1,16 @@
+from base import VtpassBase
+import requests
+
+
+class VtpassService(VtpassBase):
+    def purchase_airtime(self, payload):
+        pass
+
+    def purchase_data(self, payload):
+        pass
+
+    def purchase_electricity(self, payload):
+        pass
+
+    def purchase_tv_subscription(self, payload):
+        pass
