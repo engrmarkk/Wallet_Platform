9e14164884a5de1b3f618d3df3a655e8efefdee3
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-02-14 00:15:43 +0100

    Update __init__.py

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,3 +1,3 @@
 from routes.auth import auth as AuthenticationBlueprint
 from routes.other import view as ViewBlueprint
-from bills_payment import bills as BillsBlueprint
+from routes.bills_payment import bills as BillsBlueprint
