b51de62638d8a4323c79b6e927ffc2cb593ec462
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-02-18 16:21:31 +0100

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,11 +1,11 @@
 def determine_purchase_type(service_id):
-    if service_id in ["mtn", "glo", "etisalat", "airtel"]:
+    if service_id.lower() in ["mtn", "glo", "etisalat", "airtel"]:
         return "airtime"
-    elif service_id in ["mtn-data", "glo-data", "etisalat-data", "airtel-data", "smiles", "spectranet"]:
+    elif service_id.lower() in ["mtn-data", "glo-data", "etisalat-data", "airtel-data", "smiles", "spectranet"]:
         return "data"
-    elif service_id in ["ikedc", "ibedc"]:
+    elif service_id.lower() in ["ikedc", "ibedc", "ekedc", "kedco", "phed", "jed", "aedc", "eedc", "bedc", "aba"]:
         return "electricity"
-    elif service_id in ["cable"]:
+    elif service_id.lower() in ["dstv", "gotv", "startimes", "showmax"]:
         return "cable"
     else:
         return None
