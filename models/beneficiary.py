0c7fb841cac86f69292aa37b80892fe499e87ce4
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-02-27 22:21:47 +0100

    Update beneficiary.py

--- models/beneficiary.py
+++ models/beneficiary.py
@@ -1,15 +1,20 @@
 from extensions import db
 from flask_login import UserMixin
+import uuid
+
+
+def hexid():
+    return uuid.uuid4().hex
 
 
 # creating the User table in the database
 class Beneficiary(db.Model, UserMixin):
     __tablename__ = "beneficiary"
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.String(100), default=hexid, primary_key=True)
     first_name = db.Column(db.String(50))
     last_name = db.Column(db.String(50))
     account_number = db.Column(db.BigInteger)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    user_id = db.Column(db.String(100), db.ForeignKey("user.id"))
 
     # Define a representation with two attribute 'first_name' and 'last_name'
     def __repr__(self):
