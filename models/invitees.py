f70c2ecd52a1ed8ba63415b0d64aa770e0423f31
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-02-27 22:21:54 +0100

    Update invitees.py

--- models/invitees.py
+++ models/invitees.py
@@ -1,9 +1,14 @@
 from extensions import db
 from flask_login import UserMixin
+import uuid
+
+
+def hexid():
+    return uuid.uuid4().hex
 
 
 class Invitees(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.String(100), default=hexid, primary_key=True)
     first_name = db.Column(db.String(120))
     last_name = db.Column(db.String(120))
-    invited_by = db.Column(db.BigInteger, db.ForeignKey("user.id"))
+    invited_by = db.Column(db.String(100), db.ForeignKey("user.id"))
