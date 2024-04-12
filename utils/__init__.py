57d4ddf8a2e07b6c46ecc319eb9a44289800c0e8
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-04-12 08:13:15 +0100

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,7 +3,9 @@ def determine_purchase_type(service_id):
         return "airtime"
     elif service_id.lower() in ["mtn-data", "glo-data", "etisalat-data", "airtel-data", "smiles", "spectranet"]:
         return "data"
-    elif service_id.lower() in ["ikedc", "ibedc", "ekedc", "kedco", "phed", "jed", "aedc", "eedc", "bedc", "aba"]:
+    elif service_id.lower() in ["ikeja-electric", "eko-electric", "abuja-electric", "kano-electric", "portharcourt-electric",
+                                "jos-electric", "kaduna-electric", "enugu-electric", "ibadan-electric", "benin-electric",
+                                "aba-electric", "yola-electric"]:
         return "electricity"
     elif service_id.lower() in ["dstv", "gotv", "startimes", "showmax"]:
         return "cable"
