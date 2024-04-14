c76c16db6cb04b47dc51aa9b719e9b2c04e58bca
Adeniyi Olanrewaju <100721103+engrmarkk@users.noreply.github.com>
2024-04-14 22:36:07 +0100

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,13 +2,16 @@ from flask_mail import Message
 from extensions import mail
 from flask import render_template, flash
 
+
 def determine_purchase_type(service_id):
     if service_id.lower() in ["mtn", "glo", "etisalat", "airtel"]:
         return "airtime"
     elif service_id.lower() in ["mtn-data", "glo-data", "etisalat-data", "airtel-data", "smiles", "spectranet"]:
         return "data"
-    elif service_id.lower() in ["ikeja-electric", "eko-electric", "abuja-electric", "kano-electric", "portharcourt-electric",
-                                "jos-electric", "kaduna-electric", "enugu-electric", "ibadan-electric", "benin-electric",
+    elif service_id.lower() in ["ikeja-electric", "eko-electric", "abuja-electric", "kano-electric",
+                                "portharcourt-electric",
+                                "jos-electric", "kaduna-electric", "enugu-electric", "ibadan-electric",
+                                "benin-electric",
                                 "aba-electric", "yola-electric"]:
         return "electricity"
     elif service_id.lower() in ["dstv", "gotv", "startimes", "showmax"]:
@@ -17,7 +20,7 @@ def determine_purchase_type(service_id):
         return None
 
 
-def send_notification(current_user, amount, user1, x):
+def send_notification(current_user, amount, description, x, phone):
     try:
         msg = Message(
             subject="DEBIT ALERT",
@@ -25,9 +28,10 @@ def send_notification(current_user, amount, user1, x):
             recipients=[current_user.email],
         )
         msg.html = render_template(
-            "debit.html",
+            "debit_bills.html",
             amount=f"{amount:,.2f}",
-            user=user1,
+            description=description,
+            phone=phone,
             balance=f"{current_user.account_balance:,}",
             date=x,
             acct=str(current_user.account_number),
