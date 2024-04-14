b614908b2260c795d2a6beed947f5473a439ca84
Adeniyi Olanrewaju <100721103+engrmarkk@users.noreply.github.com>
2024-04-14 22:30:24 +0100

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,7 @@
+from flask_mail import Message
+from extensions import mail
+from flask import render_template, flash
+
 def determine_purchase_type(service_id):
     if service_id.lower() in ["mtn", "glo", "etisalat", "airtel"]:
         return "airtime"
@@ -11,3 +15,24 @@ def determine_purchase_type(service_id):
         return "cable"
     else:
         return None
+
+
+def send_notification(current_user, amount, user1, x):
+    try:
+        msg = Message(
+            subject="DEBIT ALERT",
+            sender="EasyTransact <easytransact.send@gmail.com>",
+            recipients=[current_user.email],
+        )
+        msg.html = render_template(
+            "debit.html",
+            amount=f"{amount:,.2f}",
+            user=user1,
+            balance=f"{current_user.account_balance:,}",
+            date=x,
+            acct=str(current_user.account_number),
+        )
+        mail.send(msg)
+    except Exception as e:
+        print(e, "ERROR")
+        flash("Network Error", "danger")
