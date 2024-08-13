53e89e9127733e4f1bb4bd2fe530842c836e2012
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-08-13 21:16:33 +0100

    Update app_config.py

--- app_config.py
+++ app_config.py
@@ -68,6 +68,11 @@ def create_app():
     def page_not_found(e):
         return render_template('404.html'), 404
 
+    # 500
+    @app.errorhandler(500)
+    def internal_server_error(e):
+        return render_template('500.html'), 500
+
     @app.before_request
     def before_request():
         session.permanent = True
