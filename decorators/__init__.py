2c4820f39212fd79f97e1211697e76565e7755d1
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-12-03 10:04:36 +0100

    Create __init__.py

---
+++ decorators/__init__.py
@@ -0,0 +1,23 @@
+from func import wraps
+from models import Admin
+from flask_login import current_user
+from flask import redirect, url_for
+
+
+def admin_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not Admin.query.filter_by(id=current_user.id).first():
+            return redirect(url_for("view.home"))
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+def super_admin_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        admin = Admin.query.filter_by(id=current_user.id).first()
+        if not admin or not admin.is_super_admin:
+            return redirect(url_for("view.home"))
+        return f(*args, **kwargs)
+    return decorated_function
