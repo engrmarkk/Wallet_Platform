8533eb83f9e7e186d5f1260a20fe7c1782c56cf0
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-12-03 10:04:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from app_config import create_app, db
 from extensions import sess
-from models import User, Invitees, Beneficiary, Transaction, Card, TransactionCategories, UserSession
+from models import User, Invitees, Beneficiary, Transaction, Card, TransactionCategories, UserSession, Admin
 from dotenv import load_dotenv
 
 
@@ -12,7 +12,8 @@ load_dotenv()
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Invitees': Invitees, 'Beneficiary': Beneficiary, 'Transaction': Transaction,
-            'Card': Card, 'TransactionCategories': TransactionCategories, 'UserSession': UserSession}
+            'Card': Card, 'TransactionCategories': TransactionCategories, 'UserSession': UserSession,
+            'Admin': Admin}
 
 
 if __name__ == "__main__":
