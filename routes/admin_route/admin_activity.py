94d8375d13e64f8fdd5fa94360d8e15294a86ae4
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-12-08 16:21:47 +0100

    Update admin_activity.py

--- routes/admin_route/admin_activity.py
+++ routes/admin_route/admin_activity.py
@@ -1,20 +1,14 @@
 from flask import Blueprint, render_template, request, url_for, session, redirect
 from flask_login import login_required, current_user
-from func import get_all_admins, create_admin, create_super_admin, get_all_users, get_one_user, get_one_admin
+from func import get_all_admins, create_admin, create_super_admin, get_all_users, get_one_user, get_one_admin,\
+    get_user_transactions
 import traceback
 from extensions import db
 from decorators import super_admin_required, admin_required
-import string
-import random
 
 
 admin_blp = Blueprint("admin_blp", __name__, template_folder="../templates")
 
-def generate_ran_str():
-    return ''.join(random.choices(string.ascii_letters + string.digits, k=5))
-
-strin = generate_ran_str()
-check_str = strin
 
 # get admins
 @admin_blp.route("/admins", methods=["GET", "POST"])
@@ -94,20 +88,25 @@ def get_users():
         user_name = request.args.get("user_name")
         account_number = request.args.get("account_number")
         has_set_panic = request.args.get("has_set_panic")
-        users = get_all_users(page, per_page, email, fullname, phone_number, user_name, account_number, has_set_panic)
-        return render_template("admin_temp/all_users.html", users=users, alert=alert, bg_color=bg_color)
+        users, user_count, completed_user, active_users = get_all_users(page, per_page, email, fullname, phone_number, user_name, account_number, has_set_panic)
+        return render_template("admin_temp/all_users.html", users=users, alert=alert, bg_color=bg_color,
+                               user_count=user_count, all_users=True, completed_user=completed_user,
+                               uncompleted_user=user_count - completed_user, active_users=active_users,
+                               inactive_users=user_count - active_users)
     except Exception as e:
         print(e, "error in get users")
         print(traceback.format_exc(), "TraceBack")
-        return redirect(url_for("view.home"))
+        return redirect(url_for("admin_blp.get_users"))
 
 
 # update user
 @admin_blp.route("/user/<user_id>", methods=["GET"])
 @login_required
-@super_admin_required
+# @super_admin_required
 def one_user(user_id):
     try:
+        alert = session.pop("alert", None)
+        bg_color = session.pop("bg_color", None)
         user = get_one_user(user_id)
         deactivate = request.args.get("deactivate", None)
         if not user:
@@ -117,30 +116,51 @@ def one_user(user_id):
         if deactivate:
             user.active = not user.active
             db.session.commit()
-            session["alert"] = "User updated successfully"
+            session["alert"] = "User deactivated successfully" if not user.active else "User activated successfully"
             session["bg_color"] = "success"
-            return redirect(url_for("admin_blp.get_users"))
-        return render_template("admin_temp/one_user.html", user=user)
+            return redirect(url_for("admin_blp.one_user", user_id=user.id))
+        return render_template("admin_temp/one_user.html", user=user, all_users=True,
+                               alert=alert, bg_color=bg_color)
     except Exception as e:
         print(e, "error in one user")
         print(traceback.format_exc(), "TraceBack")
-        return redirect(url_for("view.home"))
+        return redirect(url_for("admin_blp.one_user", user_id=user_id))
+
+
+# user transaction
+@admin_blp.route("/user/<user_id>/transactions", methods=["GET"])
+@login_required
+def user_transactions(user_id):
+    try:
+        page = int(request.args.get("page", 1))
+        per_page = int(request.args.get("per_page", 10))
+        transaction_type = request.args.get("transaction_type")
+        status = request.args.get("status")
+        category = request.args.get("category")
+        user = get_one_user(user_id)
+        if not user:
+            session["alert"] = "User not found"
+            session["bg_color"] = "danger"
+            return redirect(url_for("admin_blp.one_user", user_id=user_id))
+        paginated_transactions, all_transactions, successful_transactions, pending_transactions, failed_transactions, inflow_transactions, outflow_transactions = get_user_transactions(page, per_page, transaction_type, status, category, user)
+        return render_template("admin_temp/user_transactions.html", user=user, all_users=True,
+                               paginated_transactions=paginated_transactions, all_transactions_count=all_transactions,
+                               successful_transactions=successful_transactions, pending_transactions=pending_transactions,
+                               failed_transactions=failed_transactions, inflow_transactions=inflow_transactions,
+                               outflow_transactions=outflow_transactions)
+    except Exception as e:
+        print(e, "error in user transactions")
+        print(traceback.format_exc(), "TraceBack")
+        return redirect(url_for("admin_blp.one_user", user_id=user_id))
 
 
 # admin dashboard
-@admin_blp.route(f"/dashboard?s={strin}", methods=["GET"])
+@admin_blp.route(f"/dashboard", methods=["GET"])
 @login_required
 def admin_dashboard():
     try:
-        print(strin, "strin")
-        print(check_str, "check_str")
-        sess = 0
-        print(sess, "sess")
-        if strin != check_str and not sess:
-            return redirect(url_for("view.home"))
         if not current_user.is_admin:
             return redirect(url_for("view.home"))
-        sess += 1
         return render_template("admin_temp/admin_dashboard.html", admin_dashboard=True)
     except Exception as e:
         print(e, "error in admin dashboard")
