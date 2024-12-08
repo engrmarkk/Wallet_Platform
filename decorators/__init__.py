6d94a3c4322cbfc6aefdfa11769b488ba1f3e748
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-12-08 22:52:55 +0100

    Update __init__.py

--- decorators/__init__.py
+++ decorators/__init__.py
@@ -21,3 +21,12 @@ def super_admin_required(f):
             return redirect(url_for("view.home"))
         return f(*args, **kwargs)
     return decorated_function
+
+
+def user_admin_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not current_user.is_admin:
+            return redirect(url_for("view.home"))
+        return f(*args, **kwargs)
+    return decorated_function
