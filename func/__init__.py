589bd28253b861088fa80d175bbc7dc5dd4b35db
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-12-14 20:52:55 +0100

    Update __init__.py

--- func/__init__.py
+++ func/__init__.py
@@ -25,7 +25,9 @@ def save_image(form_picture):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
-    picture_path = os.path.join(current_app.root_path, 'static\\images\\profile', picture_fn)
+    picture_path = os.path.join(
+        current_app.root_path, "static\\images\\profile", picture_fn
+    )
 
     output_size = (1200, 1200)
     i = Image.open(form_picture)
@@ -35,8 +37,18 @@ def save_image(form_picture):
     return picture_fn
 
 
-trans_cats = ["Transfer", "Airtime", "Electricity", "Tv-Subscription",
-              "Internet-Data", "W2W", "Savings", "Referral", "Wallet-Topup", "Transfer"]
+trans_cats = [
+    "Transfer",
+    "Airtime",
+    "Electricity",
+    "Tv-Subscription",
+    "Internet-Data",
+    "W2W",
+    "Savings",
+    "Referral",
+    "Wallet-Topup",
+    "Transfer",
+]
 
 # W2W stands for wallet to wallet
 
@@ -61,17 +73,27 @@ def get_cat(cat):
 
 
 def return_cat_type(purchase_type):
-    if purchase_type == 'airtime':
-        return 'Airtime'
-    elif purchase_type == 'electricity':
-        return 'Electricity'
-    elif purchase_type == 'cable':
-        return 'Tv-Subscription'
-    elif purchase_type == 'data':
-        return 'Internet-Data'
+    if purchase_type == "airtime":
+        return "Airtime"
+    elif purchase_type == "electricity":
+        return "Electricity"
+    elif purchase_type == "cable":
+        return "Tv-Subscription"
+    elif purchase_type == "data":
+        return "Internet-Data"
 
 
-def deduct_history(amount, current_user, request_id, purchase_type, service_id, phone="", customer_name="", verify_number="", panic=False):
+def deduct_history(
+    amount,
+    current_user,
+    request_id,
+    purchase_type,
+    service_id,
+    phone="",
+    customer_name="",
+    verify_number="",
+    panic=False,
+):
     print(purchase_type, "purchase type")
     print("got here@deduct")
 
@@ -97,7 +119,7 @@ def deduct_history(amount, current_user, request_id, purchase_type, service_id, 
         category=get_cat(cat),
         user_id=current_user.id,
         customer_name=customer_name,
-        panic_mode=panic
+        panic_mode=panic,
     )
     db.session.add(transact)
     db.session.commit()
@@ -122,11 +144,18 @@ def refund(amount, current_user, request_id, purchase_type, service_id, phone=""
         description="Refund for " + cat + "/" + service_id,
         status="Refunded",
         user_id=current_user.id,
-        panic_mode=True if current_user.panic_mode else False
+        panic_mode=True if current_user.panic_mode else False,
     )
     db.session.add(transact)
     db.session.commit()
-    send_credit_notification("REFUND", current_user, amount, transact.description, transact.date_posted, phone)
+    send_credit_notification(
+        "REFUND",
+        current_user,
+        amount,
+        transact.description,
+        transact.date_posted,
+        phone,
+    )
     return transact
 
 
@@ -147,8 +176,13 @@ def update_status(transact, status):
 def create_admin(first_name, last_name, email, password, is_super_admin=False):
     try:
         print("creating admin with is super admin", is_super_admin)
-        admin = Admin(first_name=first_name, last_name=last_name, email=email,
-                      password=hasher.hash(password), is_super_admin=is_super_admin)
+        admin = Admin(
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            password=hasher.hash(password),
+            is_super_admin=is_super_admin,
+        )
         db.session.add(admin)
         db.session.commit()
         return admin
@@ -178,8 +212,13 @@ def get_all_admins(page, per_page, email, is_super_admin, fullname, phone):
         if phone:
             admins = admins.filter(Admin.phone_number.ilike(f"%{phone}%"))
         if fullname:
-            admins = admins.filter(Admin.first_name.ilike(f"%{fullname}%") | Admin.last_name.ilike(f"%{fullname}%"))
-        paginated_admins = admins.order_by(Admin.date.desc()).paginate(page=page, per_page=per_page, error_out=False)
+            admins = admins.filter(
+                Admin.first_name.ilike(f"%{fullname}%")
+                | Admin.last_name.ilike(f"%{fullname}%")
+            )
+        paginated_admins = admins.order_by(Admin.date.desc()).paginate(
+            page=page, per_page=per_page, error_out=False
+        )
         return paginated_admins
     except Exception as e:
         print(e, "error in get_all_admins")
@@ -187,7 +226,16 @@ def get_all_admins(page, per_page, email, is_super_admin, fullname, phone):
         return None
 
 
-def get_all_users(page, per_page, email, fullname, phone_number, user_name, account_number, has_set_panic):
+def get_all_users(
+    page,
+    per_page,
+    email,
+    fullname,
+    phone_number,
+    user_name,
+    account_number,
+    has_set_panic,
+):
     try:
         users = User.query
         user_count = users.count()
@@ -197,7 +245,8 @@ def get_all_users(page, per_page, email, fullname, phone_number, user_name, acco
             users = users.filter(User.email.ilike(f"%{email}%"))
         if fullname:
             users = users.filter(User.first_name.ilike(f"%{fullname}%")) | users.filter(
-                User.last_name.ilike(f"%{fullname}%"))
+                User.last_name.ilike(f"%{fullname}%")
+            )
         if phone_number:
             users = users.filter(User.phone_number.ilike(f"%{phone_number}%"))
         if user_name:
@@ -206,7 +255,9 @@ def get_all_users(page, per_page, email, fullname, phone_number, user_name, acco
             users = users.filter(User.account_number.ilike(f"%{account_number}%"))
         if has_set_panic:
             users = users.filter(User.has_set_panic == True)
-        paginated_users = users.order_by(User.date_joined.desc()).paginate(page=page, per_page=per_page, error_out=False)
+        paginated_users = users.order_by(User.date_joined.desc()).paginate(
+            page=page, per_page=per_page, error_out=False
+        )
         return paginated_users, user_count, completed_user, active_users
     except Exception as e:
         print(e, "error in get_all_users")
@@ -239,26 +290,50 @@ def get_user_transactions(page, per_page, transaction_type, status, category, us
     try:
         transactions = Transaction.query.filter(Transaction.user_id == user.id)
         all_transactions = transactions.count()
-        successful_transactions = transactions.filter(Transaction.status == "Success").count()
-        pending_transactions = transactions.filter(Transaction.status == "Pending").count()
-        failed_transactions = transactions.filter(Transaction.status == "Failed").count()
-        inflow_transactions = transactions.filter(Transaction.transaction_type == "CRT").count()
-        outflow_transactions = transactions.filter(Transaction.transaction_type == "DBT").count()
+        successful_transactions = transactions.filter(
+            Transaction.status == "Success"
+        ).count()
+        pending_transactions = transactions.filter(
+            Transaction.status == "Pending"
+        ).count()
+        failed_transactions = transactions.filter(
+            Transaction.status == "Failed"
+        ).count()
+        inflow_transactions = transactions.filter(
+            Transaction.transaction_type == "CRT"
+        ).count()
+        outflow_transactions = transactions.filter(
+            Transaction.transaction_type == "DBT"
+        ).count()
         if transaction_type:
-            transactions = transactions.filter(Transaction.transaction_type == transaction_type)
+            transactions = transactions.filter(
+                Transaction.transaction_type == transaction_type
+            )
         if status:
             transactions = transactions.filter(Transaction.status == status)
         if category:
             transactions = transactions.filter(Transaction.category == category)
-        paginated_transactions = transactions.order_by(Transaction.date_posted.desc()).paginate(page=page, per_page=per_page, error_out=False)
-        return paginated_transactions, all_transactions, successful_transactions, pending_transactions, failed_transactions, inflow_transactions, outflow_transactions
+        paginated_transactions = transactions.order_by(
+            Transaction.date_posted.desc()
+        ).paginate(page=page, per_page=per_page, error_out=False)
+        return (
+            paginated_transactions,
+            all_transactions,
+            successful_transactions,
+            pending_transactions,
+            failed_transactions,
+            inflow_transactions,
+            outflow_transactions,
+        )
     except Exception as e:
         print(e, "error in get_user_transactions")
         db.session.rollback()
         return None, None, None, None, None, None, None
 
 
-def get_all_transactions(page, per_page, status, transaction_type, category, bank_name, receiver):
+def get_all_transactions(
+    page, per_page, status, transaction_type, category, bank_name, receiver
+):
     try:
         transactions = Transaction.query
         all_trans_count = transactions.count()
@@ -270,16 +345,31 @@ def get_all_transactions(page, per_page, status, transaction_type, category, ban
         if status:
             transactions = transactions.filter(Transaction.status.ilike(status))
         if transaction_type:
-            transactions = transactions.filter(Transaction.transaction_type.ilike(transaction_type))
+            transactions = transactions.filter(
+                Transaction.transaction_type.ilike(transaction_type)
+            )
         if category:
             transactions = transactions.filter(Transaction.category == category)
         if bank_name:
-            transactions = transactions.filter(Transaction.bank_name.ilike(f"%{bank_name}%"))
+            transactions = transactions.filter(
+                Transaction.bank_name.ilike(f"%{bank_name}%")
+            )
         if receiver:
-            transactions = transactions.filter(Transaction.receiver.ilike(f"%{receiver}%"))
-        paginated_transactions = transactions.order_by(Transaction.date_posted.desc()).paginate(page=page,
-                                                                                                per_page=per_page, error_out=False)
-        return paginated_transactions, all_trans_count, success_counts, pending_counts, failed_counts, inflow, outflow
+            transactions = transactions.filter(
+                Transaction.receiver.ilike(f"%{receiver}%")
+            )
+        paginated_transactions = transactions.order_by(
+            Transaction.date_posted.desc()
+        ).paginate(page=page, per_page=per_page, error_out=False)
+        return (
+            paginated_transactions,
+            all_trans_count,
+            success_counts,
+            pending_counts,
+            failed_counts,
+            inflow,
+            outflow,
+        )
     except Exception as e:
         print(e, "error in get_all_transactions")
         db.session.rollback()
@@ -299,47 +389,62 @@ def statistics():
 
         # Aggregate transactions
         transaction_stats = {
-            "all": safe_first(Transaction.query.with_entities(
-                func.count(), func.sum(Transaction.transaction_amount)
-            ).first()),
-            "success": safe_first(Transaction.query.filter(Transaction.status == "Success").with_entities(
-                func.count(), func.sum(Transaction.transaction_amount)
-            ).first()),
-            "pending": safe_first(Transaction.query.filter(Transaction.status == "Pending").with_entities(
-                func.count(), func.sum(Transaction.transaction_amount)
-            ).first()),
-            "failed": safe_first(Transaction.query.filter(Transaction.status == "Failed").with_entities(
-                func.count(), func.sum(Transaction.transaction_amount)
-            ).first()),
-            "inflow": safe_first(Transaction.query.filter(Transaction.transaction_type == "CRT").with_entities(
-                func.count(), func.sum(Transaction.transaction_amount)
-            ).first()),
-            "outflow": safe_first(Transaction.query.filter(Transaction.transaction_type == "DBT").with_entities(
-                func.count(), func.sum(Transaction.transaction_amount)
-            ).first()),
+            "all": safe_first(
+                Transaction.query.with_entities(
+                    func.count(), func.sum(Transaction.transaction_amount)
+                ).first()
+            ),
+            "success": safe_first(
+                Transaction.query.filter(Transaction.status == "Success")
+                .with_entities(func.count(), func.sum(Transaction.transaction_amount))
+                .first()
+            ),
+            "pending": safe_first(
+                Transaction.query.filter(Transaction.status == "Pending")
+                .with_entities(func.count(), func.sum(Transaction.transaction_amount))
+                .first()
+            ),
+            "failed": safe_first(
+                Transaction.query.filter(Transaction.status == "Failed")
+                .with_entities(func.count(), func.sum(Transaction.transaction_amount))
+                .first()
+            ),
+            "inflow": safe_first(
+                Transaction.query.filter(Transaction.transaction_type == "CRT")
+                .with_entities(func.count(), func.sum(Transaction.transaction_amount))
+                .first()
+            ),
+            "outflow": safe_first(
+                Transaction.query.filter(Transaction.transaction_type == "DBT")
+                .with_entities(func.count(), func.sum(Transaction.transaction_amount))
+                .first()
+            ),
         }
 
         # Transaction counts and amounts by category
         categories = [
-            "Electricity", "Airtime", "Internet-Data",
-            "Tv-Subscription", "W2W", "Transfer", "Wallet-Topup"
+            "Electricity",
+            "Airtime",
+            "Internet-Data",
+            "Tv-Subscription",
+            "W2W",
+            "Transfer",
+            "Wallet-Topup",
         ]
 
         category_stats = {}
         for category in categories:
-            count, amount = (
-                db.session.query(
-                    func.count(),
-                    func.sum(Transaction.transaction_amount)
-                )
-                .join(TransactionCategories, Transaction.category == TransactionCategories.id)
-                .filter(TransactionCategories.category == category)
-                .first() or (0, 0)
+            count, amount = db.session.query(
+                func.count(), func.sum(Transaction.transaction_amount)
+            ).join(
+                TransactionCategories, Transaction.category == TransactionCategories.id
+            ).filter(
+                TransactionCategories.category == category
+            ).first() or (
+                0,
+                0,
             )
-            category_stats[category] = {
-                "count": count,
-                "amount": amount or 0
-            }
+            category_stats[category] = {"count": count, "amount": amount or 0}
 
         # Transactions per day
         current_date = date.today()
@@ -366,7 +471,7 @@ def statistics():
             "outflow_amount": transaction_stats["outflow"][1] or 0,
             **category_stats,
             "transactions_today_count": transactions_today[0],
-            "transactions_today_amount": transactions_today[1] or 0
+            "transactions_today_amount": transactions_today[1] or 0,
         }
 
         return results
