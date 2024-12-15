0263ae4611bba110bd439f160c70524a2e5460f5
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-12-15 20:53:16 +0100

    Update auth.py

--- routes/auth.py
+++ routes/auth.py
@@ -51,8 +51,13 @@ def validate(email):
             alert = "Try again, invalid code"
             bg_color = "danger"
 
-    return render_template("confirmation.html", email=email, date=datetime.utcnow(),
-                           alert=alert, bg_color=bg_color)
+    return render_template(
+        "confirmation.html",
+        email=email,
+        date=datetime.utcnow(),
+        alert=alert,
+        bg_color=bg_color,
+    )
 
 
 @auth.route("/login/", methods=["GET", "POST"])
@@ -87,14 +92,20 @@ def login():
                             sender="Easytransact <easytransact.send@gmail.com>",
                             recipients=[email],
                         )
-                        msg.html = render_template("email_verification.html", otp=str(otp))
+                        msg.html = render_template(
+                            "email_verification.html", otp=str(otp)
+                        )
                         mail.send(msg)
                     except Exception as e:
                         print(e, "ERROR")
                         alert = "Failed to validate"
                         bg_color = "danger"
                         return render_template(
-                            "login.html", date=datetime.utcnow(), form=form, alert=alert, bg_color=bg_color
+                            "login.html",
+                            date=datetime.utcnow(),
+                            form=form,
+                            alert=alert,
+                            bg_color=bg_color,
                         )
 
                     return redirect(url_for("auth.validate", email=email))
@@ -105,9 +116,15 @@ def login():
                     if user.has_set_panic:
                         if check_password_hash(user.panic_password, form.password.data):
                             if user.enabled_2fa:
-                                return render_template("login.html", date=datetime.utcnow(),
-                                                       form=form, alert=alert, bg_color=bg_color,
-                                                       email=email, open_modal=True)
+                                return render_template(
+                                    "login.html",
+                                    date=datetime.utcnow(),
+                                    form=form,
+                                    alert=alert,
+                                    bg_color=bg_color,
+                                    email=email,
+                                    open_modal=True,
+                                )
                             # If the check passed, login the user and flash a message to the user when redirected to the
                             # homepage
                             session["alert"] = "Login Successful"
@@ -120,9 +137,15 @@ def login():
 
                     if check_password_hash(user.password, form.password.data):
                         if user.enabled_2fa:
-                            return render_template("login.html", date=datetime.utcnow(),
-                                                   form=form, alert=alert, bg_color=bg_color,
-                                                   email=email, open_modal=True)
+                            return render_template(
+                                "login.html",
+                                date=datetime.utcnow(),
+                                form=form,
+                                alert=alert,
+                                bg_color=bg_color,
+                                email=email,
+                                open_modal=True,
+                            )
                         # If the check passed, login the user and flash a message to the user when redirected to the
                         # homepage
                         session["alert"] = "Login Successful"
@@ -165,22 +188,38 @@ def login():
 
         # This for a get request, if u click on the link that leads to the login page, this return statement get
         # called upon
-        return render_template("login.html", date=datetime.utcnow(),
-                               form=form, alert=alert, bg_color=bg_color, open_modal=False)
+        return render_template(
+            "login.html",
+            date=datetime.utcnow(),
+            form=form,
+            alert=alert,
+            bg_color=bg_color,
+            open_modal=False,
+        )
 
     except OperationalError as e:
         print(e, "ERROR")
         alert = "Please check your internet connection"
         bg_color = "danger"
-        return render_template("login.html", date=datetime.utcnow(), form=form,
-                               alert=alert, bg_color=bg_color)
+        return render_template(
+            "login.html",
+            date=datetime.utcnow(),
+            form=form,
+            alert=alert,
+            bg_color=bg_color,
+        )
 
     except Exception as e:
         print(e, "ERROR")
         alert = "Something went wrong"
         bg_color = "danger"
-        return render_template("login.html", date=datetime.utcnow(), form=form,
-                               alert=alert, bg_color=bg_color)
+        return render_template(
+            "login.html",
+            date=datetime.utcnow(),
+            form=form,
+            alert=alert,
+            bg_color=bg_color,
+        )
 
 
 # verify_2fa
@@ -192,16 +231,28 @@ def verify_2fa():
     if not auth_code:
         alert = "Please enter the authentication code"
         bg_color = "danger"
-        return render_template("login.html", date=datetime.utcnow(),
-                               form=form, alert=alert, bg_color=bg_color,
-                               email=email, open_modal=True)
+        return render_template(
+            "login.html",
+            date=datetime.utcnow(),
+            form=form,
+            alert=alert,
+            bg_color=bg_color,
+            email=email,
+            open_modal=True,
+        )
     user = User.query.filter_by(email=email).first()
     if not authenticate_auth_code(user, auth_code):
         alert = "Invalid authentication code"
         bg_color = "danger"
-        return render_template("login.html", date=datetime.utcnow(),
-                               form=form, alert=alert, bg_color=bg_color,
-                               email=email, open_modal=True)
+        return render_template(
+            "login.html",
+            date=datetime.utcnow(),
+            form=form,
+            alert=alert,
+            bg_color=bg_color,
+            email=email,
+            open_modal=True,
+        )
     session["alert"] = "Login Successful"
     session["bg_color"] = "success"
     login_user(user, remember=False)
@@ -237,8 +288,11 @@ def register():
                 alert = "User with this email already exist"
                 bg_color = "danger"
                 return render_template(
-                    "register.html", date=datetime.utcnow(), form=form,
-                    alert=alert, bg_color=bg_color
+                    "register.html",
+                    date=datetime.utcnow(),
+                    form=form,
+                    alert=alert,
+                    bg_color=bg_color,
                 )
 
             # Check if phone number exist
@@ -254,8 +308,11 @@ def register():
                 alert = "This is not a valid number"
                 bg_color = "danger"
                 return render_template(
-                    "register.html", date=datetime.utcnow(), form=form,
-                    alert=alert, bg_color=bg_color
+                    "register.html",
+                    date=datetime.utcnow(),
+                    form=form,
+                    alert=alert,
+                    bg_color=bg_color,
                 )
 
             first_name = form.first_name.data.lower()
@@ -279,8 +336,11 @@ def register():
                 alert = "Password should contain at least an uppercase, lowercase and a number"
                 bg_color = "danger"
                 return render_template(
-                    "register.html", date=datetime.utcnow(), form=form,
-                    alert=alert, bg_color=bg_color
+                    "register.html",
+                    date=datetime.utcnow(),
+                    form=form,
+                    alert=alert,
+                    bg_color=bg_color,
                 )
 
             if len(phone_number) != 11:
@@ -297,8 +357,11 @@ def register():
                 alert = "Invalid referral code"
                 bg_color = "danger"
                 return render_template(
-                    "register.html", date=datetime.utcnow(), form=form,
-                    alert=alert, bg_color=bg_color
+                    "register.html",
+                    date=datetime.utcnow(),
+                    form=form,
+                    alert=alert,
+                    bg_color=bg_color,
                 )
 
             if not form.invited_by.data:
@@ -331,8 +394,11 @@ def register():
                 alert = "Failed to verify email"
                 bg_color = "danger"
                 return render_template(
-                    "register.html", date=datetime.utcnow(), form=form,
-                    alert=alert, bg_color=bg_color
+                    "register.html",
+                    date=datetime.utcnow(),
+                    form=form,
+                    alert=alert,
+                    bg_color=bg_color,
                 )
 
             # Add the 'new_user'
@@ -343,8 +409,13 @@ def register():
             session["bg_color"] = "success"
             return redirect(url_for("auth.validate", email=email))
 
-    return render_template("register.html", date=datetime.utcnow(), form=form,
-                           alert=alert, bg_color=bg_color)
+    return render_template(
+        "register.html",
+        date=datetime.utcnow(),
+        form=form,
+        alert=alert,
+        bg_color=bg_color,
+    )
 
 
 @auth.route("/logout/")
