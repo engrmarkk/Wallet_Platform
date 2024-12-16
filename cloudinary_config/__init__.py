05b70304976757fa43caeec362ac0afef5371534
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-12-16 21:05:12 +0100

    Update __init__.py

--- cloudinary_config/__init__.py
+++ cloudinary_config/__init__.py
@@ -4,5 +4,5 @@ import os
 cloudinary.config(
     cloud_name=os.getenv("CLOUD_NAME"),
     api_key=os.getenv("API_KEY"),
-    api_secret=os.getenv("API_SECRET")
+    api_secret=os.getenv("API_SECRET"),
 )
