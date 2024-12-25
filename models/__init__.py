b8f57e09e21f93d3443a034e99debfd662ce8629
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-12-25 13:30:08 +0100

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,10 @@
 from models.main import User, UserSession
 from models.transact import Transaction, Card, TransactionCategories
-from models.beneficiary import Beneficiary, save_bank_beneficiary, BankBeneficiary, get_one_bank_beneficiary
+from models.beneficiary import (
+    Beneficiary,
+    save_bank_beneficiary,
+    BankBeneficiary,
+    get_one_bank_beneficiary,
+)
 from models.invitees import Invitees
 from models.admin import Admin
