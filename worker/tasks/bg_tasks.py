b1054bb34331bffb51caead1023822dbbbcdefb6
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2026-03-11 09:57:22 +0100

    tasks

---
+++ worker/tasks/bg_tasks.py
@@ -0,0 +1,25 @@
+from flask_mail import Message
+from flask import render_template
+from extensions import mail
+from celery import shared_task
+
+
+@shared_task
+def send_email_users(subject, email, template_name, html_context: dict, send_body = None):
+    try:
+        msg = Message(
+            subject=subject,
+            sender="Easytransact <easytransact.send@gmail.com>",
+            recipients=[email] if isinstance(email, str) else email,
+        )
+        if send_body:
+            msg.body = send_body
+        else:
+            msg.html = render_template(
+                template_name, **html_context
+            )
+        mail.send(msg)
+        return "Sent successfully"
+    except Exception as e:
+        print(f"Error from sending mail: {e}")
+        return None
