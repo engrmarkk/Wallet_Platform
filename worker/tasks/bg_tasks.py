14b7c51d138fe09cb76e068d335e093fb30d10d3
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2026-03-11 10:50:29 +0100

    Update bg_tasks.py

--- worker/tasks/bg_tasks.py
+++ worker/tasks/bg_tasks.py
@@ -5,9 +5,11 @@ from celery import shared_task
 
 
 @shared_task
-def send_email_users(subject, email, template_name, html_context: dict, send_body = None):
+def send_email_users(subject, email, template_name, html_context: dict, send_body=None):
     try:
-        print(f"Got notification for subject: {subject}: template_name: {template_name}")
+        print(
+            f"Got notification for subject: {subject}: template_name: {template_name}"
+        )
         msg = Message(
             subject=subject,
             sender="Easytransact <easytransact.send@gmail.com>",
@@ -16,9 +18,7 @@ def send_email_users(subject, email, template_name, html_context: dict, send_bod
         if send_body:
             msg.body = send_body
         else:
-            msg.html = render_template(
-                template_name, **html_context
-            )
+            msg.html = render_template(template_name, **html_context)
         mail.send(msg)
         return "Sent successfully"
     except Exception as e:
